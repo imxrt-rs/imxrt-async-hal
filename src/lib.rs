78fc8986058c47f8d79bf40aff3d92e13e1a8435
Ian McIntyre <ianpmcintyre@gmail.com>
2020-10-07 20:17:57 -0400

    Remove chip-specific crate; go back to features
    Still not sure how it's going to work with documentation examples.
    Let's keep the feature-flag approach until we see how it goes in
    the imxrt-hal crate.

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,9 @@ mod uart;
 
 pub use imxrt_ral as ral;
 
+#[cfg(target_arch = "arm")]
+use cortex_m_rt as rt;
+
 //
 // Module re-exports
 //
@@ -81,6 +84,35 @@ pub struct IPGClock {
 }
 
 /// Pad multiplexing and configuration
-mod iomuxc {
+///
+/// The `iomuxc` module is a re-export of the [`imxrt-iomuxc`] crate. It combines
+/// the i.MX RT processor-specific components with the `imxrt-iomuxc` general API.
+/// It then adds a safe function, [`take`](fn.take.html), which lets you convert
+/// the RAL's `iomuxc::Instance` into all of the processor [`Pads`](struct.Pads.html).
+///
+/// ```no_run
+/// use imxrt_async_hal as hal;
+/// use hal::{ral::iomuxc::IOMUXC, iomuxc};
+///
+/// let pads = iomuxc::new(IOMUXC::take().unwrap());
+/// ```
+///
+/// `Pads` can then be used in peripheral-specific APIs.
+///
+/// [`imxrt-iomuxc`]: https://docs.rs/imxrt-iomuxc/0.1/imxrt_iomuxc/
+pub mod iomuxc {
+    #[cfg(features = "imxrt106x")]
+    pub use imxrt_iomuxc::imxrt106x::*;
     pub use imxrt_iomuxc::prelude::*;
+
+    /// Turn the `IOMUXC` instance into pads
+    ///
+    /// See the [module-level docs](index.html) for an example.
+    #[cfg(any(features = "imxrt106x"))]
+    pub fn new(_: crate::ral::iomuxc::Instance) -> Pads {
+        // Safety: ^--- there's a single instance. Either the user
+        // used an `unsafe` method to steal it, or we own the only
+        // instance.
+        unsafe { Pads::new() }
+    }
 }
